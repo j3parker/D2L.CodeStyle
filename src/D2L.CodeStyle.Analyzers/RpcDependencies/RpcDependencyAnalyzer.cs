87e2fafe885050eecadc680aa68d629d4ac8569a
Jacob Parker <Jacob.Parker@D2L.com>
2017-04-11 16:58:20 -0400

    Add check for sort order of [Dependency] arguments

--- src/D2L.CodeStyle.Analyzers/RpcDependencies/RpcDependencyAnalyzer.cs
+++ src/D2L.CodeStyle.Analyzers/RpcDependencies/RpcDependencyAnalyzer.cs
@@ -18,7 +18,27 @@ namespace D2L.CodeStyle.Analyzers.RpcDependencies {
 			description: "RPCs must take an IRpcContext as their first argument"
 		);
 
-		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create( RpcContextRule );
+		internal static readonly DiagnosticDescriptor SortRule = new DiagnosticDescriptor(
+			id: "D2L0005",
+			title: "Dependency-injected arguments must be properly sorted",
+			messageFormat: "Dependency-injected must come before RPC parameters but after IRpcContext",
+			category: "Correctness",
+			defaultSeverity: DiagnosticSeverity.Error,
+			isEnabledByDefault: true,
+			description: "Dependency-injected must come before RPC parameters but after IRpcContext"
+		);
+
+		internal static readonly DiagnosticDescriptor DependencyAttributeRule = new DiagnosticDescriptor(
+			id: "D2L0006",
+			title: "Argument cannot come from RPC parameters. Are you missing a [Dependency] attribute?",
+			messageFormat: "Argument cannot come from RPC parameters. Are you missing a [Dependency] attribute?",
+			category: "Correctness",
+			defaultSeverity: DiagnosticSeverity.Error,
+			isEnabledByDefault: true,
+			description: "Argument cannot come from RPC parameters. Are you missing a [Dependency] attribute?"
+		);
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create( RpcContextRule, SortRule );
 
 		public override void Initialize( AnalysisContext context ) {
 			context.RegisterSyntaxNodeAction( AnalyzeMethod, SyntaxKind.MethodDeclaration );
@@ -31,39 +51,75 @@ namespace D2L.CodeStyle.Analyzers.RpcDependencies {
 				return;
 			}
 
+			var ps = method.ParameterList.Parameters;
+
+			bool done = CheckThatFirstArgumentIsIRpcContext( context, method, ps );
+
+			if ( done ) {
+				return;
+			}
+
+			CheckThatDependencyArgumentsAreSortedCorrectly( context, ps );
+
+			// other things to check:
+			// - appropriate use of [Dependency]
+		}
+
+		private static bool CheckThatFirstArgumentIsIRpcContext(
+			SyntaxNodeAnalysisContext context,
+			MethodDeclarationSyntax method,
+			SeparatedSyntaxList<ParameterSyntax> ps
+		) {
 			// Could this be cached per-compilation?
 			var rpcAttributeType = context.Compilation.GetTypeByMetadataName( "D2L.Web.RpcAttribute" );
 
 			bool isRpc = method
 				.AttributeLists
 				.SelectMany( al => al.Attributes )
-				.Any( attr => IsRpcAttribute( rpcAttributeType, attr, context.SemanticModel ) );
+				.Any( attr => IsAttribute( rpcAttributeType, attr, context.SemanticModel ) );
 
 			if( !isRpc ) {
-				return;
+				return true;
 			}
-
-			var ps = method.ParameterList.Parameters;
 
 			if ( ps.Count == 0 ) {
 				context.ReportDiagnostic( Diagnostic.Create( RpcContextRule, method.ParameterList.GetLocation() ) );
-				return;
-			} else {
-				var firstParam = method.ParameterList.Parameters[0];
-
-				var rpcContextType = context.Compilation.GetTypeByMetadataName( "D2L.Web.IRpcContext" );
-
-				if ( !ParameterIsOfTypeIRpcContext( rpcContextType, firstParam, context.SemanticModel ) ) {
-					context.ReportDiagnostic( Diagnostic.Create( RpcContextRule, firstParam.GetLocation() ) );
-				}
+				return true;
 			}
 
-			// other things to check:
-			// - sort order of [Dependency] arguments
-			// - appropriate use of [Dependency]
+			var firstParam = method.ParameterList.Parameters[0];
+
+			var rpcContextType = context.Compilation.GetTypeByMetadataName( "D2L.Web.IRpcContext" );
+
+			if ( !ParameterIsOfTypeIRpcContext( rpcContextType, firstParam, context.SemanticModel ) ) {
+				context.ReportDiagnostic( Diagnostic.Create( RpcContextRule, firstParam.GetLocation() ) );
+			}
+
+			return false;
 		}
 
-		private static bool IsRpcAttribute( INamedTypeSymbol expectedType, AttributeSyntax attr, SemanticModel model ) {
+		private static void CheckThatDependencyArgumentsAreSortedCorrectly(
+			SyntaxNodeAnalysisContext context,
+			SeparatedSyntaxList<ParameterSyntax> ps
+		) {
+			var dependencyAttributeType = context.Compilation.GetTypeByMetadataName( "D2L.LP.Extensibility.Activation.Domain.DependencyAttribute" );
+
+			bool doneDependencies = false;
+			foreach( var param in ps.Skip( 1 ) ){
+				var isDep = param
+					.AttributeLists
+					.SelectMany( al => al.Attributes )
+					.Any( attr => IsAttribute( dependencyAttributeType, attr, context.SemanticModel ) );
+
+				if( !isDep && !doneDependencies ) {
+					doneDependencies = true;
+				} else if ( isDep && doneDependencies ) {
+					context.ReportDiagnostic( Diagnostic.Create( SortRule, param.GetLocation() ) );
+				}
+			}
+		}
+
+		private static bool IsAttribute( INamedTypeSymbol expectedType, AttributeSyntax attr, SemanticModel model ) {
 			var symbol = model.GetSymbolInfo( attr ).Symbol;
 
 			if ( symbol == null || symbol.Kind == SymbolKind.ErrorType ) {
--- tests/D2L.CodeStyle.Analyzers.Test/RpcDependencies/RpcDependencyAnalyzerTests.cs
+++ tests/D2L.CodeStyle.Analyzers.Test/RpcDependencies/RpcDependencyAnalyzerTests.cs
@@ -51,7 +51,7 @@ namespace Test {
 		public void Test() {}
 	}
 }";
-			AssertSingleDiagnostic( test, 15, 19 );
+			AssertSingleDiagnostic( RpcDependencyAnalyzer.RpcContextRule, test, 15, 19 );
 		}
 
 		[Test]
@@ -65,7 +65,7 @@ namespace Test {
 		public void Test( int x ) {}
 	}
 }";
-			AssertSingleDiagnostic( test, 15, 21 );
+			AssertSingleDiagnostic( RpcDependencyAnalyzer.RpcContextRule, test, 15, 21 );
 		}
 
 		[Test]
@@ -82,14 +82,89 @@ namespace Test {
 			AssertNoDiagnostic( test );
 		}
 
+		[Test]
+		public void RpcWithoutDependencyArgs_NoDiag() {
+			const string test = PREAMBLE + @"
+namespace Test {
+	using D2L.Web;
+	using D2L.LP.Extensibility.Activation.Domain;
+
+	class Test {
+		[Rpc]
+		public void Test( IRpcContext x, int x, int y ) {}
+	}
+}";
+			AssertNoDiagnostic( test );
+		}
+
+		[Test]
+		public void RpcWithSingleDependencyNoParams_NoDiag() {
+			const string test = PREAMBLE + @"
+namespace Test {
+	using D2L.Web;
+	using D2L.LP.Extensibility.Activation.Domain;
+
+	class Test {
+		[Rpc]
+		public void Test( IRpcContext x, [Dependency] int x ) {}
+	}
+}";
+			AssertNoDiagnostic( test );
+		}
+
+		[Test]
+		public void RpcWithTwoDependencyNoParams_NoDiag() {
+			const string test = PREAMBLE + @"
+namespace Test {
+	using D2L.Web;
+	using D2L.LP.Extensibility.Activation.Domain;
+
+	class Test {
+		[Rpc]
+		public void Test( IRpcContext x, [Dependency] int x, [Dependency] int y ) {}
+	}
+}";
+			AssertNoDiagnostic( test );
+		}
+
+		[Test]
+		public void RpcWithTwoDependencySingleParams_NoDiag() {
+			const string test = PREAMBLE + @"
+namespace Test {
+	using D2L.Web;
+	using D2L.LP.Extensibility.Activation.Domain;
+
+	class Test {
+		[Rpc]
+		public void Test( IRpcContext x, [Dependency] int x, [Dependency] int y, int z ) {}
+	}
+}";
+			AssertNoDiagnostic( test );
+		}
+
+		[Test]
+		public void RpcWithTwoDependencySingleParamInTheMiddle_SortDiag() {
+			const string test = PREAMBLE + @"
+namespace Test {
+	using D2L.Web;
+	using D2L.LP.Extensibility.Activation.Domain;
+
+	class Test {
+		[Rpc]
+		public void Test( IRpcContext x, [Dependency] int x, int y, [Dependency] int z ) {}
+	}
+}";
+			AssertSingleDiagnostic( RpcDependencyAnalyzer.SortRule, test, 16, 63 );
+		}
+
 		private void AssertNoDiagnostic( string file ) {
 			VerifyCSharpDiagnostic( file );
 		}
 
-        private void AssertSingleDiagnostic( string file, int line, int column ) {
+		private void AssertSingleDiagnostic( DiagnosticDescriptor diag, string file, int line, int column ) {
 			DiagnosticResult result = new DiagnosticResult {
-				Id = RpcDependencyAnalyzer.RpcContextRule.Id,
-				Message = RpcDependencyAnalyzer.RpcContextRule.MessageFormat.ToString(),
+				Id = diag.Id,
+				Message = diag.MessageFormat.ToString(),
 				Severity =  DiagnosticSeverity.Error,
 				Locations = new [] {
 					new DiagnosticResultLocation( "Test0.cs", line, column )
